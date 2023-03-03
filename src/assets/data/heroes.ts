8a0caba2e24ac27deecbdbe3974c4c91f6855935
rheera <rheera2@uwo.ca>
2023-03-02 21:29:03 -0500

    initial commit

---
+++ src/assets/data/heroes.ts
@@ -0,0 +1,172 @@
+export const heroes = [
+  {
+    name: 'D.Va',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Doomfist',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Orisa',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Reinhardt',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Roadhog',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Sigma',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Winston',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Wrecking Ball',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Zarya',
+    role: 'Tank',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Ashe',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Bastion',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Cassidy',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Echo',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Genji',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Hanzo',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Junkrat',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Mei',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Pharah',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Reaper',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Sojourn',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Soldier: 76',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Sombra',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Symmetra',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Torbjörn',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Tracer',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Widowmaker',
+    role: 'Damage',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Ana',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Baptiste',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Brigitte',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Lúcio',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Mercy',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Moira',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Zenyatta',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  },
+  {
+    name: 'Kiriko',
+    role: 'Support',
+    img: '/assets/images/headshots/dva.png'
+  }
+]
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
+<style lang="scss">
+.app-wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+}
+</style>
